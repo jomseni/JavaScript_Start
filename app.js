cc370a5961807d355757b05e4a8e209c60490125
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-29 07:56:25 +0900

    list 생성과 요소 추가하기
     여러 개의 변수를 묶음을 하는 List를 공부(생성 하는 법 , 요소 추가 하는 법)

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('hi');
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri","sat"];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[4]); // fri 출력
+
+// Add one more day o the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
