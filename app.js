197f19d625171592382b78a4791142a5b483260d
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-29 08:19:09 +0900

    Object개체 만들어 여러 기능 수행
    개체를 만든 후 값을 수정, 추가 ,조회 등을 공부해본다!
    
    배열은 그 배열의 동일한 기준에 의한 값들을 알 수 있지만, 값 마다 설명하는 것이 다를 경우 이렇게 개체(Entity)를 생성하여 정의하여 준다

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri","sat"];
+//  개체 만들기(object)
+//  단순하게 변수나 배열을 이용할 경우에 표현이 너무 복잡하거나 배열안의 요소에 대해 그 값이 무엇을 의미하는지
+// 설명이필요할 경우가 있기 때문이다!
 
-console.log(daysOfWeek);
-console.log(daysOfWeek[4]); // fri 출력
+const player = {
+    name : "park",
+    points : 10,
+    fat : true,
+};
 
-// Add one more day o the array
-daysOfWeek.push("sun");
+// console도 개체라는 것을 알 수 있고, 그 뒤의 log는 개체 안에 들어있는 값이라는 것을 알 수있다.
+console.log(player);
+console.log(player.name);
 
-console.log(daysOfWeek);
+// 객체의 값을 바꿔줄 수 있다
+player.fat = false;
+
+console.log(player);
+
+// const는 수정이 안된다 했었는데 왜 가능할까?
+// const안의 값은 수정이가능하다! 그러나 const player의 player 전체의 의미를 바꾸는건 오류가 발생!
+//  예를들면 player = false 이런식으로 수정을 하면 오류가 발생 한다!
+
+
+// 개체안에 값을 추가하는 방법!
+player.lastName = "potato";
+
+// 값을 추가 하여 수정하는 것도 가능하다!
+player.points = player.points + 15;
+
+console.log(player);
