4005fb84cb5605a3c6e4bc96857afb26da48fe43
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-31 01:10:18 +0900

    toggle함수
    classList.toggle() => toggle은 h1의 classList에 clicked class가 이미 있는지 확인해서 있다면 ,toggle이  clicked를 제거하고 만약 h1의 classList에 clicked가 존재하지 않다면
        // toggle이 clicked를 classList에 추가해준다.

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-// JS를 통해서 HTML element를 가져와서 h1 객체로 대입
 const h1 = document.querySelector("div.hello:first-child h1");
-//  click event 함수
-function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){  //if문의 조건에 들어가는거니깐 ===(3개) 일치연산자
-        newColor = "tomato";  //대입연산자 = 하나
-    }else{
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
-}
-h1.addEventListener("click",handleTitleClick);
 
+function handleTitleClick(){
+    // classList.toggle() => toggle은 h1의 classList에 clicked class가 이미 있는지 확인해서 있다면 ,toggle이  clicked를 제거하고 만약 h1의 classList에 clicked가 존재하지 않다면 
+    // toggle이 clicked를 classList에 추가해준다.
+    h1.classList.toggle("clicked");  
+} 
+h1.addEventListener("click",handleTitleClick);
 
