d482b0c015dec981cee03a7c43c8e5ff3ee74ab5
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-31 01:11:42 +0900

    애플리케이션 만들기 시작!
    JS에서 스타일 작업을 하는건 비효율적이다
    
    스타일 작업에 있어서 적절한 도구는 CSS이기 때문에 스타일작업은 CSS를 이용하자!
    
    애니메이션에 적합한 도구는 JS이다.

--- app.js
+++
@@ -1,9 +0,0 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick(){
-    // classList.toggle() => toggle은 h1의 classList에 clicked class가 이미 있는지 확인해서 있다면 ,toggle이  clicked를 제거하고 만약 h1의 classList에 clicked가 존재하지 않다면 
-    // toggle이 clicked를 classList에 추가해준다.
-    h1.classList.toggle("clicked");  
-} 
-h1.addEventListener("click",handleTitleClick);
-
