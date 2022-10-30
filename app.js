4fa5e0d9b45461c9045d46a908199bd959834e0d
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-30 17:25:46 +0900

    조건문 배우기
    isNaN()함수는 boolean형태의 값을 반환하고 이 반환 값을 이용해
    조건문을 수행해본다!
    조건문은 if(condition){}else{}를 이용해
    경우에 따라 수행 문을 다르게 정의할 수 있다. condition이 true이면 if문 내부의 실행 문을 수행하고 condition이 false이면 else문 내부의 코드를 수행한다.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-// 키보드 값은 문자이다.
-// 키보드 값 문자로 받은 후 parseInt()를 이용하여 정수형으로 변수를 바꿔준 호 age 변수에 대입
-const age = parseInt( prompt("How old are you?") );
- 
+//  NaN (Not a Number) ==> 숫자가 아니다!
 
-// 1. 숫자를 입력할 경우,숫자 정수가 출력 된다!
-// 2. 문자를 입력할 경우, 첫번 째 출력에서 숫자가 아니다!(NaN) 나옴
-console.log(age);
-
- 
- 
+const age = parseInt(prompt("How old are you?"));
 
 
-//  입력 값의 타입을 보는 방법! console.log(typeof 변수, typeof 변수)
+// 키보드의 입력 값을 age로 받아 이 입력 값이 NaN인지 판별할 수 있는 것을 배울 것이다!
+// isNaN() 함수를 통해 그것을 판별할 수 있고 반환 값은 boolean값인 true 와 false로 반환 될 것이다
+//  false = 숫자, true => 숫자가 아닌 다른 타입! 왜냐하면 NaN은 Not  a Number 이므로!
+
+
+//15  입력 시에 false , '문자' 입력 시에 => true  출력!
+console.log(isNaN(age));
+
+
+// if 문의 조건은 boolean 형식으로 반환되어야 한다.
+// isNaN()함수가 참이면 if 문 수행, false 이면 else 문 수행!
+if(isNaN(age)){
+//  condition == true
+console.log("Please write a number!!!");
+}else{
+// condition == false
+    console.log("Thank you for writing you age!");
+}
