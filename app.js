66e20fb6035daa909d93d4d6f4df9dceafe7af20
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-30 16:12:12 +0900

    개체 생성하고 개체안에 함수정의 해서 계산기 만들어 보기
    개체 안에 함수 명 : function(매개변수a,매개변수b)를 통해서 수의 연산이 가능한 실행 문 코드를 작성하여 계산기를 만들기!

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-//  개체를 만들고 개체가 함수를 수행하는 것을 만들어 볼 것이다!
-//  예를들면 console.log()와 동일한 모양의 코드!
+// 계산기 만들기! 개체 안에 함수를 추가해서 만들어보기!
 
-
-// 개체만들기
-const player = {
+const calculator = {
     name : "nico",
-    // sayHello라는 player안의 함수를 부르기위해 개체 생성 안에 함수를 정의 및 생성해준다.
-    sayHello : function(otherPersonName){ // 아래에서 보낸 데이터를 받아서 함수 실행문 실행!
-        console.log("hello!" + otherPersonName + " nice to meet you");
+    add : function(a,b){
+        console.log(a + b);
+    },
+    
+    min : function(a,b){
+        console.log(a-b);
+    },
+
+    mul : function(a,b){
+        console.log(a *b);
+    },
+
+    pof : function(a,b){
+        console.log(a**b);
+    },
+
+    div : function(a,b){
+        console.log(a / b);
     }
 };
 
-console.log(player.name);
-player.sayHello("lynn");  //데이터(argument)를 보내서 함수가 매개변수(parameter)로 데이터로 받아 함수를 수행
+calculator.add(5,1);  //6
+calculator.min(4,1);  //3 
+calculator.mul(3,2);  // 6
+calculator.pof(2,5);  // 32
+calculator.div(8,2);   //4
