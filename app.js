911a4211fc20b95237c9a3b9517183bc3ee73479
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-30 22:33:14 +0900

    이벤트 사용하는 방법과 개념
    event 종류들은 console.dir(element)로 검색한 후 많은 목록 중에서 on- 으로 시작하는 것들이 event들 이다.
    on을 뺀 단어를 addEventListener("a",b)에서 a 자리에 넣어주면 되며, b자리엔 이벤트 함수의 이름을 작성하되, 함수지만 ()를 빼고 작성한다. 왜냐하면 사용자가 event를 발생하면 js가 실행시켜주기위함이다.

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-
-////  HTML의 요소를 JavaScript로 가져오기!
-const title = document.querySelector("div.hello:first-child h1");   // <h1>content</h1> 을 가져온다 
-
-//envent를 listen하는 방법!
+// JS를 통해서 HTML element를 가져와서 title 객체로 대입
+const title = document.querySelector("div.hello:first-child h1");
 
 
-
-//누군가가 tite을 click 했을 때 수행할 행동을 정의하기 위한 함수 생성
+//  click event 함수
 function handleTitleClick(){
     title.style.color = "blue";
 }
 
-//js에서 title을 지켜보다가 누군가가 title을 click하는 것을 listen할 것이다
-//event가 발생하면 실행 할 함수를 js가 실행 해준다
-title.addEventListener("click",handleTitleClick);
+
+//마우스 올려놓으면 event 발생하는 함수
+function handleMouseEnter(){
+    title.innerText = "Mouse is here!";
+}
+
 
 //
-// handleTitleClick();  () 괄호는 실행버튼을 누른다! 라고 해석하면 편한다! 여기선 내가 직접 실행버튼을 누르는게 아니라 click이 감지되면 js가 대신 클릭을 해주는 것을 원한다!
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
+
+
+// js가 listen하고 있는 event들
+// 앞에 event 종류를 써주는 부분은 console.dir(title)에서 찾을 때 on- 으로 시작하는 것들이 event 종류들이다! 거기서 앞에 on을 빼고 적어주면 된다!
+
+title.addEventListener("click",handleTitleClick);           // 누군가 title을 클릭하면 뒤에함수를 실행시켜~
+title.addEventListener("mouseenter",handleMouseEnter);      // 누군가 title에 마우스를 올려놓으면 뒤에 함수를 실행시켜라~
+title.addEventListener("mouseleave",handleMouseLeave);      // 누군가 title에서 마우스를 멀어지게하면 뒤에 함수를 실행시켜라~
+
+
+// 전체적인 실행 순서 : JS로 HTML element를 가져오고 가져온 element에 event listener를 추가해줌! 그리고 event가 발생하면 어떤 function를 시켜주면 된다!
+// 그리고 addEventListner함수 쪽엔 우리가 실행함수를 버튼을 눌러주지 않아도 된다 => 함수뒤에 ()붙이지않기! Js가 실행해주기 때문이다!
