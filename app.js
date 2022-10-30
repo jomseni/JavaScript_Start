927e6133f6be186c0358a14a999600b6a4b8094f
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-30 23:16:36 +0900

    window객체로 eventListener함수 사용하기
    JS를 통하여 document.body , document.title , document.head 를 통해 우린 HTML의 요소를 가지고 올 수있다
    나머지 element들은 querySelctor이나 getElementById 등으로 찾아와야 한다!
    
    listen하고 싶은 event 찾는 방법!
    1. 구글에 찾고 싶은 element의 이름 검색하기 ex) h1 html element mdn(mozilla developer network)
    2. 그 중에서 web APIs 이 포함된 페이지 찾기 (JS 관점의 HTML heading element란 의미)
    
    console.dir을 통해서 element를 console에 출력시키기 console.dir(title);=> 여기서도 사용가능한 event 찾을 수 있다. property 이름 앞에 on 이 있다면 그게 바로 event listener!!!! 사용할때는 on 빼고 쓰기
    
    style은 CSS를 통해서 변경되어야 한다.

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-// JS를 통해서 HTML element를 가져와서 title 객체로 대입
-const title = document.querySelector("div.hello:first-child h1");
+// JS를 통해서 HTML element를 가져와서 h1 객체로 대입
+const h1 = document.querySelector("div.hello:first-child h1");
 
 
 //  click event 함수
 function handleTitleClick(){
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
 
 //마우스 올려놓으면 event 발생하는 함수
 function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
+    h1.innerText = "Mouse is here!";
 }
 
 
 //
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato"; // html = document
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffLine(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnLine(){
+    alert("ALL GOOD!")
 }
 
 
-// js가 listen하고 있는 event들
-// 앞에 event 종류를 써주는 부분은 console.dir(title)에서 찾을 때 on- 으로 시작하는 것들이 event 종류들이다! 거기서 앞에 on을 빼고 적어주면 된다!
 
-title.addEventListener("click",handleTitleClick);           // 누군가 title을 클릭하면 뒤에함수를 실행시켜~
-title.addEventListener("mouseenter",handleMouseEnter);      // 누군가 title에 마우스를 올려놓으면 뒤에 함수를 실행시켜라~
-title.addEventListener("mouseleave",handleMouseLeave);      // 누군가 title에서 마우스를 멀어지게하면 뒤에 함수를 실행시켜라~
+// 이번엔 다른 방법으로 event를 실행시키는 코드이다!
+// 그러나 전 addEventListener()방법을 더 선호한다. 왜냐하면 나중에 removeEventListener를 통해서 event listner를 제거할 수 있기 때문이다!
 
+// h1.onclick = handleTitleClick;          // 누군가 h1을 클릭하면 뒤에함수를 실행시켜~
+// h1.onmouseenter = handleMouseEnter;      // 누군가 h1에 마우스를 올려놓으면 뒤에 함수를 실행시켜라~
+// h1.onmouseleave = handleMouseLeave;      // 누군가 h1에서 마우스를 멀어지게하면 뒤에 함수를 실행시켜라~
 
-// 전체적인 실행 순서 : JS로 HTML element를 가져오고 가져온 element에 event listener를 추가해줌! 그리고 event가 발생하면 어떤 function를 시켜주면 된다!
-// 그리고 addEventListner함수 쪽엔 우리가 실행함수를 버튼을 눌러주지 않아도 된다 => 함수뒤에 ()붙이지않기! Js가 실행해주기 때문이다!
+h1.addEventListener("click",handleTitleClick);
+h1.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseleave",handleMouseLeave);
+
+//  document 처럼 window도 기본적으로 지원해주는 객체이다
+// window 함수도 어쨌든 이벤트함수를 만들어주는 방법은 똑같다!
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy",handleWindowCopy);
+window.addEventListener("offline", handleWindowOffLine);
+window.addEventListener("online", handleWindowOnLine);
+
