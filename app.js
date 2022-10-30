1b5a10b7d30306a23c6db9d3735df0c23dd81855
jomseni <108383921+jomseni@users.noreply.github.com>
2022-10-30 15:37:23 +0900

    개체를 만들어 개체의 메서드 실행 및 데이터 보내기와 데이터를 받아와 함수 실행하기!
    console.log()와 같은 모양의 함수를 만들어 본다!
    
    player라는 개체를 만들고 그 개체 정의에 function()으로 함수를 만들어 개체.함수()로 수행하는 법을 공부해 볼 것!
    
    player 개체를 만들고 그안에 sayHello(매개변수) 함수를 정의해준다!
    
    그 후에 아래에서  개체.함수(argumen)로  호출한다!
    
    argument로 데이터를 보내고
    sayHello : function(otherPersonName)부분에서의 함수의 매개변수가 보내진 데이터를 받고 함수의 실행문을 실행한다

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-function plus(firstNumber, secondNumber){
-    console.log(firstNumber + secondNumber);
-}
+//  개체를 만들고 개체가 함수를 수행하는 것을 만들어 볼 것이다!
+//  예를들면 console.log()와 동일한 모양의 코드!
 
-function divide(a,b){
-    console.log( a / b);
-}
 
-// 함수 정의에서 사용한 매개변수의 이름은 그 함수 안에서만 사용하다!
-// 만약 firstNumber이란 변수를 함수 plus함수 밖에서 사용하면 오류가 발생한다!
+// 개체만들기
+const player = {
+    name : "nico",
+    // sayHello라는 player안의 함수를 부르기위해 개체 생성 안에 함수를 정의 및 생성해준다.
+    sayHello : function(otherPersonName){ // 아래에서 보낸 데이터를 받아서 함수 실행문 실행!
+        console.log("hello!" + otherPersonName + " nice to meet you");
+    }
+};
 
-plus(3,4);
-divide(98,20);
+console.log(player.name);
+player.sayHello("lynn");  //데이터(argument)를 보내서 함수가 매개변수(parameter)로 데이터로 받아 함수를 수행
