c9c8624831547ebeef1142030d7837c9c5d780f7
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-01 18:58:19 +0900

    이벤트 함수의 인수 event 이해하기
    이제 JS 파일의 button event 말고 form의 submit에 집중할 것이다.
    
    submit이라는 event가 발생하는 걸 아예 막거나 중간에 개입해서 submit event가 발생하는 걸 파악할 것이다!
    
    이제 JS에서의 event Button이 필요 없으므로 지울 것이다!
    
    고쳐줘야하고 공부해야할 부분은 login-firm의 이름을 가진 HTML의 form 태그이다
    
    click 이벤트가 아닌 submit이벤트에 대한 함수를 작성해준다 => onLoginSubmit()
    
    submit은 엔터를 누르거나 버튼을 클릭할 때 발생함.
    
    따라서 loginForm.addEventListener("submit", onLoginSubmit)을 작성해서 함수를 실행 시킬 수 있다.
    ( submit이벤트 발생하면 onLoginSubmit)을 JS에게 실행 시키게 한다는 뜻!
    
    이 말은 다르게 말하면 함수이름뒤에 ()를 붙이면 함수가 바로 실행되지만 그렇게 실행하게 하는게 아니라
    
    submit event가 발생하면 브라우저가 알아서 함수를 실행시켜주게 하는 것이다.(우리가 함수를 바로 실행하게하는 것이아님! JS를 이용!)
    
    이렇게 코드를 수정해주면  이제 form을 submit할 때 입력 값을 받아내는 것은 성공하였다.
    
    하지만 새로고침은 아직도 적용되어져 있다.
    
    새로고침은 form submit의 기본적인 동작이다.
    
    브라우저는 입력 값을 누르고 엔터를 누를 때 새로고침을 하고 form을 submit 하도록 되어있다는 뜻이다.
    
    그렇다면 이제 그 디폴트 행동을 발생하지 않게 하는 것이 목표 이다.
    
    JS를 이용해서 발생하지 않게 할 수 있다.
    
    함수명()을 작성 해 버리면 이벤트발생을 안해도 함수가 실행되는것이다
    
    따라서 이벤트가 발생했을 경우에만 브라우저가 함수를 실행하게 하기위해서 addEventListener()를 사용 하는 것이다!
    
    이벤트 발생 시 실행되는 함수에 인수를 넣어볼 것이다. 그럼 브라우저가 어떤정보를 넘겨주는지 확인할 수 있다.
    
    이것은 함수가 하나의 인수(argument)를 받도록 하고 그걸 JS에 넘겨주는 행위이다
    
    누군가 form을 submit하면 JS가 이 함수를 호출 하도록 하고 있는 것!
    
    함수의 하나의 인수에 event객체를 넣어 브라우저의 기본 행동을 막아 줄 수 있게 한다.
    
    인수에 아무것도 넣지 않으면 브라우저의 기본 행동을 막아줄 함수를 실행 할 수 없기 때문이다.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 //만약 querySelector()을 사용 할 경우 querySelector("#login-form") 이렇게 상세하게 써줘야함 
-// 그러나 getElementById는 Js에서 ID를 찾는 줄 알기 때문에 따로 #을 안써도 됨
+// 그러나 getElementById는 Js에서 ID를 찾는 줄 알기 때문에 따로 #을 안써도 됨 . 그러나 하위 태그를 연결하여 사용하기 위해서 쿼리셀렉터를 많이 사용한다
 // const loginForm = document.getElementById("login-form");
 // const loginInput = loginForm.querySelector("input");
 // const loginButton = loginForm.querySelector("button");
 
-const loginInput = document.querySelector("#login-form input");               //괄호 안은  ID값의 (공백) 하위 선택자(input)로서 하위에 있는 것을 나타내는 것 : 결론적으로 input이 중요
-const loginButton = document.querySelector("#login-form button");               //loginInput과 loginButton도 object(객체)이다!
 
-//클릭 이벤트 연결 (버튼을 누르면 칸에 작성한걸 추후엔 데이터 저장도 가능하게 하기 위해서!)
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    console.log(username);
+const loginForm = document.querySelector("#login-form")  //HTML의 form 태그를 끌고와서 객체로 만듦
+const loginInput = document.querySelector("#login-form input");               //괄호 안은  ID값의 (공백) 하위 선택자(input)로서 하위에 있는 것을 나타내는 것 : 결론적으로 input이 중요
+
+
+// submit 이벤트를 감지하면 발생하는 행동의 함수이다. submit은 엔터를 누르거나 버튼을 클릭할 때 발생함
+function onLoginSubmit(event){
+
+    // preventDefault 함수는 어떤 event의 기본 행동이 발생하지 않도록 막는 것이다. 따라서 form input을 submit시에 브라우저의 기본 행동인 새로고침을 막을 수 있다.
+    event.preventDefault();
+    // 인수를 넣고 이 이벤트에 대한 정보들을 가져올 수 있다.
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click",onLoginBtnClick);
+// 누군가 form을 submit하면 JS가 이 함수를 호출 하도록 하고 있는 것! JS에게 이 정보를 넘기는 것과 같다
+loginForm.addEventListener("submit",onLoginSubmit);
+
+
+ 
