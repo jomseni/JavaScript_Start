96f319698eb8820e5e0a4769e41d50f2bcf342b4
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-02 00:27:17 +0900

    시간을 문자로 받아 padStart와 padEnd를 이용하여 문자열 만들기
    string을 문자 두 개로 채우기!
    
    padStart()라는 함수가 이미 JS에 있다. padStart()와 padEnd() 함수는 문자열에서만 사용 가능!!!!!!매우중요!!!
    
    이 함수는 string한자리 문자를 두 자리로 바꾸어 주는 것! 예를 들면 1이면 01로 표시 해준다!
    
    사용 방법 : "1".padStart(2,"0");      1이라는 문자를 두 자리문자로 출력하는데 한 자리라면 앞에 "0"을 붙여서 두 자리를 만들어 출력하게 하는것!
    
    	앞 인수에 원하는 자리 수, 뒤 인수에 앞에 만들어 붙일 문자!!!!작성!!!!! 쉽다~~~
    
    padEnd() 함수도 있는데 이것은 원하는 문자를 뒤에 붙이는 것!
    
    ex ) "hello".padStart(20,"x");   =>>>> "xxxxxxxxxxxxxxxhello" 이런 식으로 출력 된다.
    
    ----------------------------------------------------------------------------------------------------------------
    
    #주의 :  `${}`; 방식으로 출력하는 것은 문자만 가능하다!!!
    
    date.getHours()는 문자가 아니라 숫자이다! 그래서 문자열 출력을 사용하기 위해 String으로 바꿔 주어야 한다.( 매우 중요!!!!!!)

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,12 @@
 const clock = document.querySelector("#clock");
 
 function getClock(){
+
     const date = new Date();
-    clock.innerText = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours} : ${minutes} : ${seconds}`;
 }
 
 // 함수를 이렇게 바로 적어버리면 함수가 바로 실행되는 버튼을 설정해준것과 같다. 왜냐하면 ()이것을 사용하면 함수를 바로 실행하게 하기 때문이다!
