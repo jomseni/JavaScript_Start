475826c4224c7e584d6f7c5489e585f3ce845399
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-01 23:51:47 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -4,6 +4,6 @@ function sayHello(){
     console.log("hello");
 }
 
-// setInterval 함수는 매 몇 초 마다 실행문을 실행 하게하는 함수이다.
-// setInterval은 두 개의 인수를 받는다. 첫 번째 인수는 실행하고자 하는 function이며, 두 번째 인수는 호출되는 function 간격을 몇 ms로 할 지 작성
-setInterval(sayHello, 5000);
+//setTimeout 함수는 두 개의 인수를 받는다. 첫 번째 인수는 먼저 호출하려고 하는 function을 넣어주고, 두 번째 인수는 얼마나 기다릴지 ms단위로 넣어주면된다.
+
+setTimeout(sayHello,5000);
