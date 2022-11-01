02303108924d50d514a89affdc4440c780f43bb3
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-01 22:32:47 +0900

    user가 DB에 등록되어 있지 않을 때와 등록되어 있을 때의 페이지 다르게 보여주기
    localStorage가 비어있으면 form부터 보여줘야 하고, localStorage에 유저정보가 있으면 form을 보여주면 안되고 h1을 보여준다.
    
    따라서 이 절차를 하기 위해선 먼저 localStorage에 유저정보 유무를 확인해야한다. (유저정보가 없다면 form을 보여주면 돼)
    
    반복되는 실행문을 하나의 함수로 정의하여 코드 길이를 줄여본다!
    paintGreetings()함수를 만들어서 DB에 목록이 있을 때 실행되는 코드를 넣어 준다

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const link = document.querySelector("a");  //HTML의 a태그를 가져와서 객
 // hidden이라는 클래스명을 두 번이상 사용하기 때문에 작성 해 준다. 오타 방지용!
 const HIDDEN_CLASSNAME = "hidden";
 
+// "username" 이라는 아이템의 이름(key)이 반복되어 지고 있어 , 항상 같게 써야하는데(직접 쓰다가) 오타를 할 수 있으므로 const 변수를 이용하여 사용 해 준다.
+// string을 작성하다가 오타가나면 자바스크립트가 알려주지않지만 , 변수명이 오타가나면 자바스크립트는 알려준다(작성할 때 나오기 때문에)
+// 자신이 생성한 string을 반복해서 사용하게 될 경우는 이런식으로 const 변수를 이용해 통일해준다.(변하지 않는 값이기 때문에 사용 하면 좋음)
+const USERNAME_KEY = "username";
+
 // submit 이벤트를 감지하면 발생하는 행동의 함수이다. submit은 엔터를 누르거나 버튼을 클릭할 때 발생함
 function onLoginSubmit(event){
 
@@ -24,21 +29,43 @@ function onLoginSubmit(event){
 
     // 이벤트 함수 수행 시에 input에 입력되는 값을 DB에 저장한다.  
     // 매개변수 앞의 username은 저장될 아이템의 이름이고, 뒤의 username은 우리가  키보드 입력 값으로 받아 올 변수이다.
-    localStorage.setItem("username", username);
-
+    localStorage.setItem(USERNAME_KEY, username);
+ 
     // classList를 통해서 클래스를 없으면 만들어주고, 이미 있는 것은 지워준다. 그러면 submit하면 HTML태그에 변화가 생긴다.
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
     //greeting.innerText = "Hello " + username; 문자열 합치기엔 이 방법도 있지만
     // 이 방법이 조금 더 많이 사용되니깐 이 방법으로 사용하자 ,string과 변수를 합치는 방법!!! 변수를 String안에 포함시키는 방법이다
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // greeting.innerText = `Hello ${username}`;
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
     
 }
 
 // 누군가 form을 submit하면 JS가 이 함수를 호출 하도록 하고 있는 것! JS에게 이 정보를 넘기는 것과 같다
-loginForm.addEventListener("submit",onLoginSubmit);
+// loginForm.addEventListener("submit",onLoginSubmit);
 
+// greeting.innerText = `Hello ${username}`;  와 greeting.classList.remove(HIDDEN_CLASSNAME); 이게 이벤트 함수 열렸을 때와 조건문에서 두 번 반복되어 사용되어있으므로 함수를 만들어 간단하게 작성해보자!
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// localStorage에 있는 유저정보를  가져와 객체로 만듦
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null){
+    //  show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    //show the greetings hidden 클래스를 지워주면서 h1을 보이게 한다!
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    // // 여기서 username 변수가 없으므로 saveUsername 으로 바꾸어 주어야한다!
+    // greeting.innerText = `Hello ${savedUsername}`;
+    // 여러 군데에서 반복되는 실행문을 함수 하나로 정의했으므로 함수를 실행하여 실행문들을 실행시킨다.
+    paintGreetings(savedUsername);
+}
+ 
 
 
  
