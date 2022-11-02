9e0fb383bc44f49395592b050bb569be15be14c0
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-02 19:14:24 +0900

    toDoList 목록 내용 작성하기
    이번엔 background 만들었을 때와 같은 방법으로 toDoList의 목록을 만들어 주었다.
    
    그러나 이번 commit 까지는 작성은  할 수 있으나 삭제가 되지는 않다, 또한 목록을 작성한 후 새로 고침을 하면 목록들이 브라우저에서 사라진다, 즉 저장이 되지 않는다는 것이다.
    
    이제 이런 부족한 기능들을 개선할 것이다.

--- js/todo.js
+++ js/todo.js
@@ -3,9 +3,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+
+// element(요소) = 태그 
+
+
 // form 태그는 전에도 했듯이 submit 이벤트를 가진다 따라서 기본 동작을 막아줘야한다.
 //  아하 그래서 지금 여기까지 form과 list만 만들고 실행 해보면 todolist칸에 뭘 작성하고 엔터를 누르면 새로고침이 되는구나!!!!!(todolist부분에 작성한게 그냥 깜빡거리고 없어진다)
-
 
 function handleToDoSubmit(event){
     event.preventDefault(); // 이거까지만 작성하고 todolit부분에 문자를 작성하고 엔터를 누르면 새로고침이 되지않는다(깜빡거리지않아). 왜냐면 이함수는 기본동작을 막아주고 있기 때문이다!
@@ -14,7 +17,23 @@ function handleToDoSubmit(event){
     // const 변수에 입력 값을 저장 해 놓았으므로 이 코드 다음엔 input의 value를 가지고 무얼 하든 newToDo에는 아무 영향이 없다.
     toDoInput.value = ""; //그리고 input 값을 빈 칸으로만들어
     console.log(newTodo, toDoList.value);  //여기서 이걸 출력하면 newTodo는 const이므로 아까 입력 값이 그대로 출력되고, toDoList.value의 값은 위 코드에서 공백으로 대입해줬으니 공백이 출력 된다.
+    paintToDo(newTodo);
 }
+
+// toDo를 그리는 역할을 해주는 함수를 만들어 준다.(HTML에 만들어 주기)
+function paintToDo(newTodo){
+    // background 에서 JS로 HTML을 만들어 body에 넣어준 것처럼 li를 만들어 줄 것이다!
+    // 앞 li는 변수 명, 뒤에 ""안에 있는 li는 태그 이다.
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    // 이렇게 변수 명만 만들었을땐 아직 이 요소들이 어디안에 위치 해있는지 알수 없고 변수만 만든 것!이제 아래코드를 추가해서 태그의 위치를 HTML에서 배치시켜보자
+    li.appendChild(span); // li는 이제 span이라는 자식을 가지게 되었다. HTML에 위치를 배치시킨다
+    span.innerText = newTodo; // 입력 값이 저장된 newTodo가 이제 span의 innertext 가 된 것! 아그리고 span을 사용 이유는 이후에 옆에 버튼을 만들것이기 때문인다 div로 만들면 한 라인을 다차지하므로 인 라인 방식이 아닌 블럭 방식인 span을 사용한 것이다!
+    toDoList.appendChild(li); //ul의 자식으로 li를 만들어 줆  이 코드를 작성해줘야 HTML에 위치에 맞게 들어간다.
+
+}
+
+
 
 // toDoForm 객체를 submit(엔터나 클릭(event)) 하면 handleToDoSubmit함수를 JS가 실행한다. 개발한 사용자가 실행시키는 것이 아니다!
 toDoForm.addEventListener("submit",handleToDoSubmit);
