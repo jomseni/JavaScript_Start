7610265169e43570ce7ec70ffd40ca2638dbcc32
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-02 11:04:05 +0900

    이미지 추가 하기 정리
    0과 어떤 수 사이의 숫자를 랜덤으로 가져오고 싶을 때 해야할 일은  Math.random() * 4(그 수를 곱하면된다) => 4개의 item일 경우!
    
    item이 5개 있는 Array에서 마지막 item을 가져오려면 4가 필요하다! (인덱스로 가져올 것이기 때문이다)

--- js/background.js
+++ js/background.js
@@ -7,15 +7,18 @@ const chosenImage = images[Math.floor(Math.random()* images.length)];
 // 여태까진 html에서 태그를 만들고 그것을  JS파일에서 끌어와서 객체로 만들어 사용만 해봤지 JS에서 만들고 HTML파일로 넣어 사용해 본 적이없다.
 // 이번엔 JS에서 만들어 HTML에 넣어 주는 방법으로 해 볼 것이다.
 //  우리의 목표는 JS에서 코드를 작성해 HTML에 images를 만드는 것이다. 왜냐하면 이미지를 랜덤으로 추가하기 위해선 JS가 필요하기 때문(HTML에선 정해진 이미지만 출력할 수 있는 코드를 작성할 수 있다.)
-const bgImage = document.createElement("img");
+const bgImage = document.createElement("img"); //여기서 image는 태그 이름
 // 문자열 만들기
-bgImage.src = `img/${chosenImage}`;  //이건 HTML에서 <img src="img/파일"> 과 같은 코드이다.
+bgImage.src = `img/${chosenImage}`;  //이건 HTML에서 <img src="img/파일"> 과 같은 코드이다. 여기서 image는 폴더 이름
 //이렇게 작성후 F12 콘솔을 보면 HTML에서 img 태그가 만들어진걸 볼 수 있다.  HTML의 img를 JavaScript로 만들기! 여기 까지 작성한건 document에 아직 존재하지 않고 JS에만 존재하는것 이것을 document의 body에 넣어줘야한다!
 
 
 //js에서 만든 img태그를 document의 body에 넣어주기!
 document.body.appendChild(bgImage);
 
+
+
+//코드라인 10,12,17을 JS파일에서 하면 HTML에서 <img src="img/이미지파일명"> 한 것과 같은 방법이다.
 
 
 
